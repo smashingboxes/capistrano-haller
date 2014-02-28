cc8e2000a2156a76c4bc4dc96f251318021a2efe
Jack Forrest <jack@jrforrest.net>
2014-02-28 14:13:42 -0500

    initial

---
+++ capistrano-haller.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |spec|
+  spec.name        = "capistrano-haller"
+  spec.version     = File.open(File.expand_path('../VERSION', __FILE__)).read
+  spec.author      = "Jack Forrest"
+  spec.email       = "jack@smashingboxes.com"
+  spec.homepage    = "https://github.com/smashingboxes/capistrano-haller"
+  spec.summary     = %q{Hall chat notifications for capistrano deploys}
+  spec.description = %q{Posts messages in hall channels after capistrano deployes}
+  spec.license     = "MIT"
+
+  spec.files         = `git ls-files`.split("\n")
+  spec.require_paths = ["lib"]
+
+  spec.add_runtime_dependency     "capistrano", "< 3.0"
+end
---
+++ lib/capistrano/haller.rb
@@ -0,0 +1,68 @@
+require 'net/https'
+require 'capistrano'
+
+configuration = if Capistrano::Configuration.respond_to?(:instance) 
+  Capistrano::Configuration.instance(:must_exist)
+else
+  Capistrano.configuration(:must_exist)
+end
+
+class HallNotifier
+  class HallApiError < StandardError; end
+  ROOT_URI = 'https://hall.com'
+  ENDPOINT = '/api/1/services/generic/'
+  
+  attr_accessor :room_key
+
+  def initialize(room_key)
+    @room_key = room_key
+  end
+
+  def deployed(branch, rails_env)
+    post(message_json("Branch #{branch} was deployed to #{rails_env}."))
+  end
+
+  protected
+
+  def full_path
+    ENDPOINT + room_key
+  end
+
+  def uri
+    URI.parse(ROOT_URI)
+  end
+
+  def post(json)
+    req = Net::HTTP::Post.new(full_path, {'Content-Type' => 'application/json'})
+    req.body = json
+
+    http  = Net::HTTP.new(uri.host, uri.port)
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    http.use_ssl = true
+    response = http.request(req)
+
+    unless ['200', '201'].include?(response.code)
+      raise HallApiError,
+        "Issue communicating with Hall API. Status: #{response.code}"
+    end
+  end
+
+  def message_json(body)
+    { title: "Capistrano Deployment",
+      message: body }.to_json
+  end
+end
+
+configuration.load do
+  namespace :hall_notify do
+    desc "Notify a hall channel of deployment"
+    task :notify_hall_room do
+      if not exists?(:hall_room_key) or hall_room_key.empty?
+        logger.important("You must set hall_room_key for hall notifications to work!")
+      else
+        HallNotifier.new(hall_room_key).deployed(branch, rails_env)
+      end
+    end
+    after 'deploy', 'hall_notify:notify_hall_room'
+  end
+end
